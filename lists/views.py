7b048cd87045e743f08fc39b5826fb5d5e847570
haidichen <chenhaidi43@163.com>
2020-01-01 17:07:47 -0600

    rename all item input ids and names. still broken

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect,render
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
+from lists.forms import ItemForm
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
@@ -11,7 +12,7 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
@@ -22,7 +23,7 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
