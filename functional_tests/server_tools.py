6e061658d410ab7b39265b7dae4bba89913b29d4
haidichen <chenhaidi43@163.com>
2020-01-03 16:22:32 -0600

    create session on server

---
+++ functional_tests/server_tools.py
@@ -0,0 +1,18 @@
+from fabric.api import run
+from fabric.context_managers import settings
+
+def _get_manage_dot_py(host):
+    cmd = '~/sites/{}/virtualenv/bin/python'.format(host)
+    arg = '~/sites/{}/source/manage.py'.format(host)
+    return cmd + ' ' + arg
+
+def reset_database(host):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string='pi@{}'.format(host)):
+        run('{} flush --noinput'.format(manage_dot_py))
+
+def create_session_on_server(host, email):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string='pi@{}'.format(host)):
+        session_key = run('{} create_session {}'.format(manage_dot_py, email))
+        return session_key.strip()
